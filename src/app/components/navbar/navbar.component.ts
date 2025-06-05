850f93b32f49c4df4647ce4ab5dacef441a4d2f6
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-06-05 19:57:45 +0300

    Add scroll detection and slide-down animation to navbar; fix login button tag in navbar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,16 +1,23 @@
-import { Component } from '@angular/core';
+import { Component , HostListener  } from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   imports: [
     RouterLink,
     RouterOutlet,
-    RouterLinkActive
+    RouterLinkActive,
+    NgClass
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isScrolled = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.pageYOffset > 50;
+  }
 }
