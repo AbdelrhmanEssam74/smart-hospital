f742731134448a2f36ebb94a1965145140feb1f3
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-06-05 19:08:02 +0300

    Refactor app component to include navbar and update routing
    - Replace auth container div with <app-navbar> in app.component.html
    - Import and add NavbarComponent to app component imports
    - Clean up and format route definitions in app.routes.ts
    - Add wildcard route redirecting to "404" page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import {Routes} from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: '', redirectTo: 'register', pathMatch: 'full' } // تغيير هنا
-];
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  // {path: '/home', redirectTo: '', pathMatch: 'full'},
+  {path: "**", redirectTo: "404", pathMatch: "full"}
+]
