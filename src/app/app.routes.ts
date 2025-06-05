e7dc68e5801d3ea8e3b71c6e3995785cd3492f33
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-06-05 19:58:11 +0300

    Integrate Bootstrap and MDB UI Kit styles and scripts; add HeroComponent route; enable smooth scroll behavior in global styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import {Routes} from '@angular/router';
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
+import {HeroComponent} from './components/hero/hero.component';
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  // {path: '/home', redirectTo: '', pathMatch: 'full'},
+  {path: '', component: HeroComponent},
+  {path: 'home', redirectTo: '', pathMatch: 'full'},
   {path: "**", redirectTo: "404", pathMatch: "full"}
 ]
