58cbc9f258cd4c2e93b42c80feaee08ea56375b5
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-06-06 19:18:09 +0300

    Refactor ChooseUsComponent by removing unused imports and cleaning up formatting

--- src/app/components/home-components/choose-us/choose-us.component.ts
+++ src/app/components/home-components/choose-us/choose-us.component.ts
@@ -1,15 +1,8 @@
-import { Component } from '@angular/core';
-import {
-  OurAvailableServicesCardComponent
-} from '../our-available-services-card/our-available-services-card.component';
-
+import {Component} from '@angular/core';
 @Component({
-  selector: 'app-choose-us',
-  imports: [
-    OurAvailableServicesCardComponent
-  ],
-  templateUrl: './choose-us.component.html',
-  styleUrl: './choose-us.component.css'
+    selector: 'app-choose-us',
+    templateUrl: './choose-us.component.html',
+    styleUrl: './choose-us.component.css'
 })
 export class ChooseUsComponent {
 
