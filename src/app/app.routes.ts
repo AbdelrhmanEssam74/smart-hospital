675590d37c57237809b4711c6063c299dc2c1679
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-06-06 03:11:15 +0300

    Set HomeComponent as default route and add Bootstrap icons CSS link to index.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import {Routes} from '@angular/router';
 import {LoginComponent} from './components/login/login.component';
 import {RegisterComponent} from './components/register/register.component';
-import {HeroComponent} from './components/hero/hero.component';
+import {HomeComponent} from './components/home/home.component';
 
 export const routes: Routes = [
+  {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: '', component: HeroComponent},
   {path: 'home', redirectTo: '', pathMatch: 'full'},
   {path: "**", redirectTo: "404", pathMatch: "full"}
 ]
