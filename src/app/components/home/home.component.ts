dc98d3fdd88a28ef62e2d2ffd87e2855443b2718
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-06-06 19:06:29 +0300

    Add ChooseUsComponent to home layout

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {HeroComponent} from '../hero/hero.component';
 import {AppointmentFormComponent} from '../appointment-form/appointment-form.component';
 import {OurAvailableServicesComponent} from '../our-available-services/our-available-services.component';
+import {ChooseUsComponent} from '../home-components/choose-us/choose-us.component';
 
 @Component({
   selector: 'app-home',
   imports: [
     HeroComponent,
     AppointmentFormComponent,
-    OurAvailableServicesComponent
+    OurAvailableServicesComponent,
+    ChooseUsComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
