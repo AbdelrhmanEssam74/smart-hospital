f53d1a8df7764da61f2dfe603850aaa89de44579
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-06-12 23:54:46 +0300

    Add NotificationComponent to app layout for user notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { GoToTopComponent } from './components/go-to-top/go-to-top.component';
 import { RouterOutlet } from '@angular/router';
+import {NotificationComponent} from './components/notification/notification.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, GoToTopComponent],
+  imports: [RouterOutlet, NavbarComponent, GoToTopComponent, NotificationComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
