543acb28fbffe37aea57187624bed7cb933651a4
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-07-13 03:42:45 +0300

    Enhance DoctorDashboard: update template and styles for improved UI, add today's appointments, recent patients, and patient statistics, and integrate API data with new appointment filtering logic.

--- src/app/components/dashboards/doctor/doctor-dashboard/doctor-dashboard.component.ts
+++ src/app/components/dashboards/doctor/doctor-dashboard/doctor-dashboard.component.ts
@@ -1,27 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { Doctor, DoctorDisplay } from '../../../../interfaces/doctor';
-import { DoctorService } from '../../../../services/doctor.service';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {DoctorNew} from '../../../../interfaces/doctor-new';
+import {HttpClient} from '@angular/common/http';
+import {AuthService} from '../../../../services/auth.service';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-doctor-dashboard',
-  imports: [RouterOutlet,RouterLink,RouterLinkActive],
   templateUrl: './doctor-dashboard.component.html',
+  imports: [
+    RouterLink
+  ],
   styleUrls: ['./doctor-dashboard.component.css']
 })
 export class DoctorDashboardComponent implements OnInit {
-    displayDoctor?: DoctorDisplay;
+  today_appointments: any
+  today_appointments_count: number = 0
+  upcoming_appointments_count: number = 0
+  recent_patient : any
+  APIUrl = 'http://127.0.0.1:8000/api/doctor/';
 
-  constructor(private doctorService: DoctorService) {}
+  constructor(private http: HttpClient, private auth: AuthService) {
+  }
 
   ngOnInit(): void {
-    const currentUser = localStorage.getItem('auth_currentUser');
-    if (currentUser) {
-      const user = JSON.parse(currentUser);
-      if (user.role_id == 2) {
-        this.displayDoctor = this.doctorService.getDoctorDisplayByUserId(user.id);
-      }
-    }
+    this.http.get<any>(this.APIUrl + 'appointments', {
+      headers: this.auth.getAuthHeaders()
+    }).subscribe(data => {
+      const appointments = data.appointments || [];
+      this.today_appointments = appointments.filter((appt: any) => this.isToday(appt.appointment_date))
+      this.recent_patient = appointments.filter((appt: any) => this.isToday(appt.appointment_date))[0].patient;
+      console.log(this.recent_patient)
+      this.today_appointments_count = appointments.filter((appt: any) => this.isToday(appt.appointment_date)).length;
+      this.upcoming_appointments_count = appointments.filter((appt: any) => this.isUpcoming(appt)).length;
+    }, (error) => console.error('Error:', error))
+  }
+
+  isToday(dateString: string): boolean {
+    const today = new Date();
+    const appointmentDate = new Date(dateString);
+    return (
+      today.getFullYear() === appointmentDate.getFullYear() &&
+      today.getMonth() === appointmentDate.getMonth() &&
+      today.getDate() === appointmentDate.getDate()
+    );
+  }
+
+  isUpcoming(date: any): boolean {
+    const today = new Date();
+    const appointmentDate = new Date(date.appointment_date);
+    today.setHours(0, 0, 0, 0);
+    appointmentDate.setHours(0, 0, 0, 0);
+    return appointmentDate.getTime() > today.getTime() && date.status === 'pending';
   }
 }
 
