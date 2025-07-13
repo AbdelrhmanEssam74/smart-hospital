2675b4a941239de1b580c0781fd059d2f55fd499
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-07-13 03:43:06 +0300

    Add DoctorHomeComponent: implement sidebar navigation, integrate doctor profile display, API-based data fetching, and basic styling with responsive layout. Include unit test setup.

---
+++ src/app/components/dashboards/doctor/doctor-home/doctor-home.component.ts
@@ -0,0 +1,36 @@
+import {Component, OnInit} from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {AuthService} from '../../../../services/auth.service';
+import {DoctorNew} from '../../../../interfaces/doctor-new';
+
+@Component({
+  selector: 'app-doctor-home',
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet
+  ],
+  templateUrl: './doctor-home.component.html',
+  styleUrl: './doctor-home.component.css'
+})
+export class DoctorHomeComponent implements OnInit{
+  // @ts-ignore
+  Doctor: DoctorNew;
+  APIUrl = 'http://127.0.0.1:8000/api/doctor/';
+
+  constructor(private http: HttpClient, private auth: AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.http.get<any>(this.APIUrl + 'appointments', {
+      headers: this.auth.getAuthHeaders()
+    }).subscribe(data => {
+      this.Doctor = data.doctor;
+      console.log(this.Doctor)
+    }, (error) => console.error('Error:', error))
+  }
+  logout(){
+    this.auth.logout()
+  }
+}
