1073f35a0786d8a180bb7aa03bdec2cc2a952c24
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-07-15 12:05:20 +0300

    Integrate AuthService in AppointmentService: replace `withCredentials` with `AuthService` for handling request headers, update API base URL to use loopback address.

--- src/app/services/appointment.service.ts
+++ src/app/services/appointment.service.ts
@@ -2,51 +2,52 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Appointment } from '../interfaces/appointment';
+import { AuthService} from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppointmentService {
-  private baseUrl = 'http://localhost:8000/api';
+  private baseUrl = 'http://127.0.0.1:8000/api';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient , private auth: AuthService) {}
   //  create a new appointment => Ahmed abdelhalim
   sendAppointmentToBackend(appointment: Appointment): Observable<any> {
     return this.http.post(`${this.baseUrl}/appointments/patient`, appointment, {
-      withCredentials: true
+      headers: this.auth.getAuthHeaders()
     });
   }
   // get all appointments for the current patient => Ahmed abdelhalim
     getAppointmentsForCurrentPatient(): Observable<Appointment[]> {
     return this.http.get<Appointment[]>(`${this.baseUrl}/appointments/mine`, {
-      withCredentials: true,
+      headers: this.auth.getAuthHeaders()
     });
   }
 // get patient id from backend => Ahmed abdelhalim
   getPatientIdFromBackend(): Observable<{ patient_id: number }> {
     return this.http.get<{ patient_id: number }>(`${this.baseUrl}/patient/id`, {
-      withCredentials: true
+      headers: this.auth.getAuthHeaders()
     });
   }
-  
+
 // get appointments by patient id => Ahmed abdelhalim
 getAppointmentsByUserId(patientId: string): Observable<any[]> {
   return this.http.get<any[]>(`${this.baseUrl}/appointments/patient/${patientId}`, {
-    withCredentials: true
+    headers: this.auth.getAuthHeaders()
   });
 }
 
 // delete an appointment by id => Ahmed abdelhalim
 deleteAppointment(id: number): Observable<any> {
     return this.http.delete(`${this.baseUrl}/appointments/patient/${id}`, {
-      withCredentials: true
+      headers: this.auth.getAuthHeaders()
     });
   }
 
   // update an appointment by id => Ahmed abdelhalim
   updateAppointment(id: number, updatedData: Partial<Appointment>): Observable<any> {
     return this.http.put(`${this.baseUrl}/appointments/patient/${id}`, updatedData, {
-      withCredentials: true
+      headers: this.auth.getAuthHeaders()
     });
   }
 }
