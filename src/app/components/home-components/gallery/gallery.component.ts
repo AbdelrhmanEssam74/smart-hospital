0e331073b9c1acebe2096bbaf3506d78f57d5195
Ahmed-abdel-halim <ahmedholim42@gmail.com>
2025-07-16 05:14:52 +0300

    edit Gallery

--- src/app/components/home-components/gallery/gallery.component.ts
+++ src/app/components/home-components/gallery/gallery.component.ts
@@ -1,25 +1,20 @@
 import { Component } from '@angular/core';
-import {OurAvailableServicesCardComponent} from '../our-available-services-card/our-available-services-card.component';
-import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-gallery',
-  imports: [
-    OurAvailableServicesCardComponent,
-    NgClass
-  ],
+  imports: [],
   templateUrl: './gallery.component.html',
-  styleUrl: './gallery.component.css'
+  styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent {
   Images = [
-    { src: '/assets/images/gallery/img1.jpeg', alt: 'Image 1', spanClass: 'span-2x2 span-small' },
-    { src: '/assets/images/gallery/img2.jpeg', alt: 'Image 2', spanClass: 'span-1x2 span-small' },
-    { src: '/assets/images/gallery/img3.jpeg', alt: 'Image 3', spanClass: 'span-small' },
-    { src: '/assets/images/gallery/img4.jpeg', alt: 'Image 4', spanClass: 'span-1x2 span-small' },
-    { src: '/assets/images/gallery/img5.jpeg', alt: 'Image 5', spanClass: 'span-small ' },
-    { src: '/assets/images/gallery/img6.jpeg', alt: 'Image 6', spanClass: 'span-2x1 span-small' },
-    { src: '/assets/images/gallery/img7.jpeg', alt: 'Image 7', spanClass: 'span-2x1 span-small' },
-    { src: '/assets/images/gallery/img8.jpeg', alt: 'Image 8', spanClass: 'span-small' }
+    { src: '/assets/images/gallery/img1.jpeg', alt: 'Image 1' },
+    { src: '/assets/images/gallery/img2.jpeg', alt: 'Image 2' },
+    { src: '/assets/images/gallery/img3.jpeg', alt: 'Image 3' },
+    { src: '/assets/images/gallery/img4.jpeg', alt: 'Image 4' },
+    { src: '/assets/images/gallery/img5.jpeg', alt: 'Image 5' },
+    { src: '/assets/images/gallery/img6.jpeg', alt: 'Image 6' },
+    { src: '/assets/images/gallery/img7.jpeg', alt: 'Image 7' },
+    { src: '/assets/images/gallery/img8.jpeg', alt: 'Image 8' }
   ];
 }
