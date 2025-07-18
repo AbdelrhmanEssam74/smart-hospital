48f7b830ba76f2c66523883e64327b8d115efbdd
Ahmed-abdel-halim <ahmedholim42@gmail.com>
2025-07-18 05:13:19 +0300

    hero

--- src/app/components/home-components/hero/hero.component.ts
+++ src/app/components/home-components/hero/hero.component.ts
@@ -4,19 +4,15 @@ import { Component } from '@angular/core';
   selector: 'app-hero',
   standalone: true,
   templateUrl: './hero.component.html',
-  imports: [
-
-  ],
-  styleUrl: './hero.component.css'
+  imports: [],
+  styleUrl: './hero.component.css',
 })
 export class HeroComponent {
-
-
   slides = [
     {
-      image: 'assets/images/banner/banner1.png',
-      title: 'Trusted Doctors, Modern Care',
-      description: 'Professional medical experts ready to serve you.'
+      image: 'assets/images/banner/banner3.png',
+      title: 'Compassionate Nursing Support',
+      description: 'Helping hands when you need them most.',
     },
     {
       image: 'assets/images/banner/banner2.png',
@@ -24,11 +20,9 @@ export class HeroComponent {
       description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at blandit dui, ut consequat ex. Vestibulum at ullamcorper leo. Praesent libero odio, gravida ut velit et, mattis laoreet metus.`,
     },
     {
-      image: 'assets/images/banner/banner3.png',
-      title: 'Compassionate Nursing Support',
-      description: 'Helping hands when you need them most.'
-    }
+      image: 'assets/images/banner/banner1.png',
+      title: 'Trusted Doctors, Modern Care',
+      description: 'Professional medical experts ready to serve you.',
+    },
   ];
-
-
 }
