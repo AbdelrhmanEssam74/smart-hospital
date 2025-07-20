9a4047ec3e97905bdddf9f87acd91e6bec7eb116
Ahmed-abdel-halim <ahmedholim42@gmail.com>
2025-07-20 15:53:25 +0300

    edit in profile parient

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -22,7 +22,6 @@ import { AdminAppointmentsComponent } from './components/dashboards/admin/admin-
 import { AdminAddslotsComponent } from './components/dashboards/admin/admin-addslots/admin-addslots.component';
 import { AdminAddUserComponent } from './components/dashboards/admin/admin-add-user/admin-add-user.component';
 import { AdminDoctorsComponent } from './components/dashboards/admin/admin-doctors/admin-doctors.component';
-import { AdminEditProfileComponent } from './components/dashboards/admin/admin-edit-profile/admin-edit-profile.component';
 import { AdminContactsComponent } from './components/dashboards/admin/admin-contact/admin-contact.component';
 
 export const routes: Routes = [
@@ -52,7 +51,6 @@ export const routes: Routes = [
       {path: 'admin-adduser',component:AdminAddUserComponent},
         {path: 'massagecontact',component: AdminContactsComponent},
 
-      {path: 'admin-edit-profile', component: AdminEditProfileComponent},
     ]
   },
   {path: "patient_profile", component: PatientProfileComponent},
