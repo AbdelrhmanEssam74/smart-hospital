87f99baf8be3018cdf541ae78eee13f1c91b8cd3
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-07-22 23:39:35 +0300

    Update app title to 'Clinic Hub', fix route formatting for readability, and add route for InactiveDoctorMessageComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,18 @@
-import {Routes} from '@angular/router';
-import {LoginComponent} from './components/login/login.component';
-import {RegisterComponent} from './components/register/register.component';
-import {HomeComponent} from './components/home/home.component';
-import {DoctorDetailsComponent} from './components/doctor-details/doctor-details.component';
-import {ListDoctorComponent} from './components/list-doctor/list-doctor.component';
-import {DoctorDashboardComponent} from './components/dashboards/doctor/doctor-dashboard/doctor-dashboard.component';
-import {AppointmentsComponent} from './components/dashboards/doctor/appointments/appointments.component';
-import {AddSlotComponent} from './components/dashboards/doctor/add-slot/add-slot.component';
-import {ProfileEditComponent} from './components/dashboards/doctor/profile-edit/profile-edit.component';
-import {PatientProfilesComponent} from './components/dashboards/doctor/patient-profiles/patient-profiles.component';
-import {AuthGuard} from './guards/auth.guard';
-import {PatientProfileComponent} from './components/dashboards/patient/patient-profile/patient-profile.component';
-import {EditProfileComponent} from './components/dashboards/patient/edit-profile/edit-profile.component';
-import {DoctorHomeComponent} from './components/dashboards/doctor/doctor-home/doctor-home.component';
+import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { DoctorDetailsComponent } from './components/doctor-details/doctor-details.component';
+import { ListDoctorComponent } from './components/list-doctor/list-doctor.component';
+import { DoctorDashboardComponent } from './components/dashboards/doctor/doctor-dashboard/doctor-dashboard.component';
+import { AppointmentsComponent } from './components/dashboards/doctor/appointments/appointments.component';
+import { AddSlotComponent } from './components/dashboards/doctor/add-slot/add-slot.component';
+import { ProfileEditComponent } from './components/dashboards/doctor/profile-edit/profile-edit.component';
+import { PatientProfilesComponent } from './components/dashboards/doctor/patient-profiles/patient-profiles.component';
+import { AuthGuard } from './guards/auth.guard';
+import { PatientProfileComponent } from './components/dashboards/patient/patient-profile/patient-profile.component';
+import { EditProfileComponent } from './components/dashboards/patient/edit-profile/edit-profile.component';
+import { DoctorHomeComponent } from './components/dashboards/doctor/doctor-home/doctor-home.component';
 import { GalleryComponent } from './components/home-components/gallery/gallery.component';
 import { ContactComponent } from './contact/contact.component';
 import { AdminHomeComponent } from './components/dashboards/admin/admin-home/admin-home.component';
@@ -26,45 +26,55 @@ import { AdminContactsComponent } from './components/dashboards/admin/admin-cont
 import { AdminEditUserComponent } from './components/dashboards/admin/admin-edit-user/admin-edit-user.component';
 import { AdminEditProfileComponent } from './components/dashboards/admin/admin-edit-profile/admin-edit-profile.component';
 import { AdminMangeDoctorComponent } from './components/dashboards/admin/admin-mange-doctor/admin-mange-doctor.component';
+import { InactiveDoctorMessageComponent } from './components/inactive-doctor-message/inactive-doctor-message.component';
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'home', redirectTo: '', pathMatch: 'full'},
-  {path: 'doctordetails/:id', component: DoctorDetailsComponent,canActivate: [AuthGuard]},
-  {path: 'doctors', component: ListDoctorComponent},
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'home', redirectTo: '', pathMatch: 'full' },
   {
-    path: 'doctor', component: DoctorHomeComponent, canActivate: [AuthGuard], children: [
-      {path: 'home', component: DoctorDashboardComponent},
-      {path: 'appointments', component: AppointmentsComponent},
-      {path: 'add-slot', component: AddSlotComponent},
-      {path: 'edit-profile', component: ProfileEditComponent},
-      {path: 'patients', component: PatientProfilesComponent},
-      {path: '', redirectTo: 'home', pathMatch: 'full'}
-    ]
+    path: 'doctordetails/:id',
+    component: DoctorDetailsComponent,
+    canActivate: [AuthGuard],
   },
-    {
-    path: 'admin', component: AdminHomeComponent, children: [
-      {path: 'admin-home', component: AdminDashboardComponent},
-      {path: 'admin-appointments', component: AdminAppointmentsComponent},
-      {path: 'admin-add-slot', component: AdminAddslotsComponent},
-      {path: 'admin-patients', component: AdminPatientsComponent},
-      {path: 'admin-doctors', component: AdminDoctorsComponent},
-      {path: 'admin-adduser',component:AdminAddUserComponent},
- {path: 'admin-edituser/:id',component:AdminEditUserComponent},
-      {path: 'admin-edit-profile', component: AdminEditProfileComponent},
-      {path: '', redirectTo: 'admin-home', pathMatch: 'full'},
-      {path: 'admin-edituser',component:AdminAddUserComponent},
+  { path: 'doctors', component: ListDoctorComponent },
+  {
+    path: 'doctor',
+    component: DoctorHomeComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'home', component: DoctorDashboardComponent },
+      { path: 'appointments', component: AppointmentsComponent },
+      { path: 'add-slot', component: AddSlotComponent },
+      { path: 'edit-profile', component: ProfileEditComponent },
+      { path: 'patients', component: PatientProfilesComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+    ],
+  },
+  {
+    path: 'admin',
+    component: AdminHomeComponent,
+    children: [
+      { path: 'admin-home', component: AdminDashboardComponent },
+      { path: 'admin-appointments', component: AdminAppointmentsComponent },
+      { path: 'admin-add-slot', component: AdminAddslotsComponent },
+      { path: 'admin-patients', component: AdminPatientsComponent },
+      { path: 'admin-doctors', component: AdminDoctorsComponent },
+      { path: 'admin-adduser', component: AdminAddUserComponent },
+      { path: 'admin-edituser/:id', component: AdminEditUserComponent },
+      { path: 'admin-edit-profile', component: AdminEditProfileComponent },
+      { path: '', redirectTo: 'admin-home', pathMatch: 'full' },
+      { path: 'admin-edituser', component: AdminAddUserComponent },
       // pending
-        {path: 'manage-pending',component:AdminMangeDoctorComponent},
-     {path: 'massagecontact',component: AdminContactsComponent},
-
-    ]
+      { path: 'manage-pending', component: AdminMangeDoctorComponent },
+      { path: 'massagecontact', component: AdminContactsComponent },
+    ],
   },
-  {path: "patient_profile", component: PatientProfileComponent},
-  { path: 'profile_edit', component: EditProfileComponent},
-  {path:'gallery',component: GalleryComponent},
-  {path:'contact', component: ContactComponent, canActivate: [AuthGuard]},
-  {path: "**", redirectTo: "404", pathMatch: "full"}
-]
+  { path: 'patient_profile', component: PatientProfileComponent },
+  { path: 'profile_edit', component: EditProfileComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'contact', component: ContactComponent, canActivate: [AuthGuard] },
+  { path: 'doctor-status', component: InactiveDoctorMessageComponent },
+  { path: '**', redirectTo: '404', pathMatch: 'full' },
+];
